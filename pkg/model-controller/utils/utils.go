6b77ea78392caa93b6b527f86fe0f468bb3123c3
HunterChen <1480811935cyt@gmail.com>
2025-06-24 16:56:46 +0800

    Fix delete bug

--- pkg/model-controller/utils/utils.go
+++ pkg/model-controller/utils/utils.go
@@ -111,8 +111,8 @@ func buildVllmModelInfer(model *registry.Model, backendIdx int) (*workload.Model
 			// model owns model infer. ModelInfer will be deleted when the model is deleted
 			OwnerReferences: []metav1.OwnerReference{
 				{
-					APIVersion: model.APIVersion,
-					Kind:       model.Kind,
+					APIVersion: model.TypeMeta.APIVersion,
+					Kind:       model.TypeMeta.Kind,
 					Name:       model.Name,
 					UID:        model.UID,
 				},
