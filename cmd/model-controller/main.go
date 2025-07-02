063d6fcac50155721750c028e714dd0e37186e85
HunterChen <1480811935cyt@gmail.com>
2025-07-02 10:43:22 +0800

    Add enableLeaderElection variable

--- cmd/model-controller/main.go
+++ cmd/model-controller/main.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"context"
-	"os"
-	"os/signal"
-	"syscall"
-
 	"github.com/spf13/pflag"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
 	clientset "matrixinfer.ai/matrixinfer/client-go/clientset/versioned"
 	"matrixinfer.ai/matrixinfer/pkg/model-controller/controller"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+// main starts model controller.
+// It will run forever until an error has occurred or the context is cancelled.
 func main() {
 	var kubeconfig string
 	var master string
 	var workers int
+	var enableLeaderElection bool
 
 	pflag.StringVar(&kubeconfig, "kubeconfig", "", "kubeconfig file path")
 	pflag.StringVar(&master, "master", "", "master URL")
 	pflag.IntVar(&workers, "workers", 5, "number of workers to run")
+	pflag.BoolVar(&enableLeaderElection, "leader-elect", false, "Enable leader election for controller. "+
+		"Enabling this will ensure there is only one active model controller. Default is false.")
 	pflag.Parse()
 
 	// create clientset
