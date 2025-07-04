65d8cac767ce335b91623e9e44eb06d516e31188
HunterChen <1480811935cyt@gmail.com>
2025-07-04 11:49:24 +0800

    Add leader election for model controller (#143)

--- cmd/model-controller/main.go
+++ cmd/model-controller/main.go
@@ -2,26 +2,46 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/spf13/pflag"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/uuid"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	"k8s.io/klog/v2"
 	clientset "matrixinfer.ai/matrixinfer/client-go/clientset/versioned"
 	"matrixinfer.ai/matrixinfer/pkg/model-controller/controller"
 )
 
+const (
+	defaultLeaseDuration   = 15 * time.Second
+	defaultRenewDeadline   = 10 * time.Second
+	defaultRetryPeriod     = 2 * time.Second
+	leaderElectionId       = "matrixinfer.model-controller"
+	inClusterNamespacePath = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+	leaseName              = "lease.matrixinfer.model-controller"
+)
+
+// main starts model controller.
+// It will run forever until an error has occurred or the context is cancelled.
 func main() {
 	var kubeconfig string
 	var master string
 	var workers int
+	var enableLeaderElection bool
 
 	pflag.StringVar(&kubeconfig, "kubeconfig", "", "kubeconfig file path")
 	pflag.StringVar(&master, "master", "", "master URL")
 	pflag.IntVar(&workers, "workers", 5, "number of workers to run")
+	pflag.BoolVar(&enableLeaderElection, "leader-elect", false, "Enable leader election for controller. "+
+		"Enabling this will ensure there is only one active model controller. Default is false.")
 	pflag.Parse()
 
 	// create clientset
@@ -30,27 +50,99 @@ func main() {
 		klog.Fatalf("build client config: %v", err)
 	}
 
-	kubeClient, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		klog.Fatalf("failed to create k8s client: %v", err)
-	}
-
-	client, err := clientset.NewForConfig(config)
-	if err != nil {
-		klog.Fatalf("failed to create Model client: %v", err)
-	}
+	kubeClient := kubernetes.NewForConfigOrDie(config)
+	client := clientset.NewForConfigOrDie(config)
 	// create Model controller
-	mic := controller.NewModelController(kubeClient, client)
+	mc := controller.NewModelController(kubeClient, client)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	// Start controller
-	go mic.Run(ctx, workers)
-	klog.Info("Started Model controller")
 
-	// Wait for interrupt signal
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	<-sigCh
-	klog.Info("existing")
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-ch
+		klog.Info("Received termination, signaling shutdown")
+		cancel()
+	}()
+	if enableLeaderElection {
+		leaderElector, err := initLeaderElector(kubeClient, mc, workers)
+		if err != nil {
+			panic(err)
+		}
+		// Start the leader elector process
+		leaderElector.Run(ctx)
+	} else {
+		go mc.Run(ctx, workers)
+		klog.Info("Started model controller without leader election")
+	}
+	<-ctx.Done()
+}
+
+// initLeaderElector inits a leader elector for leader election
+func initLeaderElector(kubeClient kubernetes.Interface, mc *controller.ModelController, workers int) (*leaderelection.LeaderElector, error) {
+	resourceLock, err := newResourceLock(kubeClient)
+	if err != nil {
+		return nil, err
+	}
+	leaderElector, err := leaderelection.NewLeaderElector(leaderelection.LeaderElectionConfig{
+		Lock:          resourceLock,
+		LeaseDuration: defaultLeaseDuration,
+		RenewDeadline: defaultRenewDeadline,
+		RetryPeriod:   defaultRetryPeriod,
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: func(ctx context.Context) {
+				go mc.Run(ctx, workers)
+				klog.Info("Started model controller as leader")
+			},
+			OnStoppedLeading: func() {
+				klog.Error("leader election lost")
+			},
+		},
+		ReleaseOnCancel: false,
+		Name:            leaderElectionId,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return leaderElector, nil
+}
+
+// newResourceLock returns a lease lock which is used to elect leader
+func newResourceLock(client kubernetes.Interface) (*resourcelock.LeaseLock, error) {
+	namespace, err := getInClusterNameSpace()
+	if err != nil {
+		return nil, err
+	}
+	// Leader id, should be unique
+	id, err := os.Hostname()
+	if err != nil {
+		return nil, err
+	}
+	id = id + "_" + string(uuid.NewUUID())
+	return &resourcelock.LeaseLock{
+		LeaseMeta: metav1.ObjectMeta{
+			Name:      leaseName,
+			Namespace: namespace,
+		},
+		Client: client.CoordinationV1(),
+		LockConfig: resourcelock.ResourceLockConfig{
+			Identity: id,
+		},
+	}, nil
+}
+
+// getInClusterNameSpace gets the namespace of model controller
+func getInClusterNameSpace() (string, error) {
+	if _, err := os.Stat(inClusterNamespacePath); os.IsNotExist(err) {
+		return "", fmt.Errorf("not running in-cluster, please specify namespace")
+	} else if err != nil {
+		return "", fmt.Errorf("error checking namespace file: %v", err)
+	}
+	// Load the namespace file and return its content
+	namespace, err := os.ReadFile(inClusterNamespacePath)
+	if err != nil {
+		return "", fmt.Errorf("error reading namespace file: %v", err)
+	}
+	return string(namespace), nil
 }
