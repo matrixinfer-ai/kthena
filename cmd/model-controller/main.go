062204d12ea03f675368f79b59d626f3f7df424d
HunterChen <1480811935cyt@gmail.com>
2025-07-16 11:12:44 +0800

    make lint

--- cmd/model-controller/main.go
+++ cmd/model-controller/main.go
@@ -18,11 +18,12 @@ package main
 
 import (
 	"context"
-	"matrixinfer.ai/matrixinfer/pkg/model-controller/utils"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"matrixinfer.ai/matrixinfer/pkg/model-controller/utils"
 
 	"github.com/spf13/pflag"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
