a134c7cb0b745c1acd3f311a9686602cae2d29bb
HunterChen <1480811935cyt@gmail.com>
2025-07-28 19:24:56 +0800

    make generate

--- client-go/applyconfiguration/registry/v1alpha1/modelbackend.go
+++ client-go/applyconfiguration/registry/v1alpha1/modelbackend.go
@@ -27,20 +27,20 @@ import (
 // ModelBackendApplyConfiguration represents a declarative configuration of the ModelBackend type for use
 // with apply.
 type ModelBackendApplyConfiguration struct {
-	Name                   *string                                  `json:"name,omitempty"`
-	Type                   *registryv1alpha1.ModelBackendType       `json:"type,omitempty"`
-	ModelURI               *string                                  `json:"modelURI,omitempty"`
-	CacheURI               *string                                  `json:"cacheURI,omitempty"`
-	EnvFrom                []v1.EnvFromSource                       `json:"envFrom,omitempty"`
-	Env                    []v1.EnvVar                              `json:"env,omitempty"`
-	MinReplicas            *int32                                   `json:"minReplicas,omitempty"`
-	MaxReplicas            *int32                                   `json:"maxReplicas,omitempty"`
-	ScalingCost            *int32                                   `json:"scalingCost,omitempty"`
-	RouteWeight            *int32                                   `json:"routeWeight,omitempty"`
-	ScaleToZeroGracePeriod *metav1.Duration                         `json:"scaleToZeroGracePeriod,omitempty"`
-	Workers                []ModelWorkerApplyConfiguration          `json:"workers,omitempty"`
-	LoraAdapters           []LoraAdapterApplyConfiguration          `json:"loraAdapters,omitempty"`
-	AutoscalingPolicy      *AutoscalingPolicySpecApplyConfiguration `json:"autoscalingPolicy,omitempty"`
+	Name                   *string                            `json:"name,omitempty"`
+	Type                   *registryv1alpha1.ModelBackendType `json:"type,omitempty"`
+	ModelURI               *string                            `json:"modelURI,omitempty"`
+	CacheURI               *string                            `json:"cacheURI,omitempty"`
+	EnvFrom                []v1.EnvFromSource                 `json:"envFrom,omitempty"`
+	Env                    []v1.EnvVar                        `json:"env,omitempty"`
+	MinReplicas            *int32                             `json:"minReplicas,omitempty"`
+	MaxReplicas            *int32                             `json:"maxReplicas,omitempty"`
+	ScalingCost            *int32                             `json:"scalingCost,omitempty"`
+	RouteWeight            *int32                             `json:"routeWeight,omitempty"`
+	ScaleToZeroGracePeriod *metav1.Duration                   `json:"scaleToZeroGracePeriod,omitempty"`
+	Workers                []ModelWorkerApplyConfiguration    `json:"workers,omitempty"`
+	LoraAdapters           []LoraAdapterApplyConfiguration    `json:"loraAdapters,omitempty"`
+	AutoscalingPolicyRef   *v1.LocalObjectReference           `json:"autoscalingPolicyRef,omitempty"`
 }
 
 // ModelBackendApplyConfiguration constructs a declarative configuration of the ModelBackend type for use with
@@ -167,10 +167,10 @@ func (b *ModelBackendApplyConfiguration) WithLoraAdapters(values ...*LoraAdapter
 	return b
 }
 
-// WithAutoscalingPolicy sets the AutoscalingPolicy field in the declarative configuration to the given value
+// WithAutoscalingPolicyRef sets the AutoscalingPolicyRef field in the declarative configuration to the given value
 // and returns the receiver, so that objects can be built by chaining "With" function invocations.
-// If called multiple times, the AutoscalingPolicy field is set to the value of the last call.
-func (b *ModelBackendApplyConfiguration) WithAutoscalingPolicy(value *AutoscalingPolicySpecApplyConfiguration) *ModelBackendApplyConfiguration {
-	b.AutoscalingPolicy = value
+// If called multiple times, the AutoscalingPolicyRef field is set to the value of the last call.
+func (b *ModelBackendApplyConfiguration) WithAutoscalingPolicyRef(value v1.LocalObjectReference) *ModelBackendApplyConfiguration {
+	b.AutoscalingPolicyRef = &value
 	return b
 }
--- client-go/applyconfiguration/registry/v1alpha1/modelspec.go
+++ client-go/applyconfiguration/registry/v1alpha1/modelspec.go
@@ -19,6 +19,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	v1 "k8s.io/api/core/v1"
 	networkingv1alpha1 "matrixinfer.ai/matrixinfer/client-go/applyconfiguration/networking/v1alpha1"
 )
 
@@ -28,7 +29,7 @@ type ModelSpecApplyConfiguration struct {
 	Name                     *string                                          `json:"name,omitempty"`
 	Owner                    *string                                          `json:"owner,omitempty"`
 	Backends                 []ModelBackendApplyConfiguration                 `json:"backends,omitempty"`
-	AutoscalingPolicy        *AutoscalingPolicySpecApplyConfiguration         `json:"autoscalingPolicy,omitempty"`
+	AutoscalingPolicyRef     *v1.LocalObjectReference                         `json:"autoscalingPolicyRef,omitempty"`
 	CostExpansionRatePercent *int32                                           `json:"costExpansionRatePercent,omitempty"`
 	ModelMatch               *networkingv1alpha1.ModelMatchApplyConfiguration `json:"modelMatch,omitempty"`
 }
@@ -68,11 +69,11 @@ func (b *ModelSpecApplyConfiguration) WithBackends(values ...*ModelBackendApplyC
 	return b
 }
 
-// WithAutoscalingPolicy sets the AutoscalingPolicy field in the declarative configuration to the given value
+// WithAutoscalingPolicyRef sets the AutoscalingPolicyRef field in the declarative configuration to the given value
 // and returns the receiver, so that objects can be built by chaining "With" function invocations.
-// If called multiple times, the AutoscalingPolicy field is set to the value of the last call.
-func (b *ModelSpecApplyConfiguration) WithAutoscalingPolicy(value *AutoscalingPolicySpecApplyConfiguration) *ModelSpecApplyConfiguration {
-	b.AutoscalingPolicy = value
+// If called multiple times, the AutoscalingPolicyRef field is set to the value of the last call.
+func (b *ModelSpecApplyConfiguration) WithAutoscalingPolicyRef(value v1.LocalObjectReference) *ModelSpecApplyConfiguration {
+	b.AutoscalingPolicyRef = &value
 	return b
 }
 
