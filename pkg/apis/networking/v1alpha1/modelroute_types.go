cc528e0132d4319fbfaf6e99991570c1cea79832
HunterChen <1480811935cyt@gmail.com>
2025-07-30 16:04:49 +0800

    rollback

--- pkg/apis/networking/v1alpha1/modelroute_types.go
+++ pkg/apis/networking/v1alpha1/modelroute_types.go
@@ -27,14 +27,12 @@ type ModelRouteSpec struct {
 	// this field could be empty, but it and  `ModelAdapters` can't both be empty.
 	//
 	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="modelName is immutable"
-	// +optional
 	ModelName string `json:"modelName,omitempty"`
 
 	// `model` in the LLM request could be lora adapter name,
 	// here is a list of Lora Adapter Names to match.
 	//
 	// +kubebuilder:validation:MaxItems=10
-	// +optional
 	LoraAdapters []string `json:"loraAdapters,omitempty"`
 
 	// An ordered list of route rules for LLM traffic. The first rule
