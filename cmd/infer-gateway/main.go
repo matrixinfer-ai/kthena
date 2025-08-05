33d58e9e4bd0dc7eb8077f4bd5dfd908067db019
YaoZengzeng <yaozengzeng@huawei.com>
2025-08-05 03:03:59 +0000

    remove enable-tls flag
    Signed-off-by: YaoZengzeng <yaozengzeng@huawei.com>

--- cmd/infer-gateway/main.go
+++ cmd/infer-gateway/main.go
@@ -31,21 +31,24 @@ import (
 
 func main() {
 	var (
-		port      string
-		enableTLS bool
-		tlsCert   string
-		tlsKey    string
+		port    string
+		tlsCert string
+		tlsKey  string
 	)
 
 	// Initialize klog flags
 	klog.InitFlags(nil)
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.StringVar(&port, "port", "8080", "Server listen port")
-	pflag.BoolVar(&enableTLS, "enable-tls", false, "Enable TLS")
 	pflag.StringVar(&tlsCert, "tls-cert", "", "TLS certificate file path")
 	pflag.StringVar(&tlsKey, "tls-key", "", "TLS key file path")
 	defer klog.Flush()
 	pflag.Parse()
+
+	if (tlsCert != "" && tlsKey == "") || (tlsCert == "" && tlsKey != "") {
+		klog.Fatal("tls-cert and tls-key must be specified together")
+	}
+
 	pflag.CommandLine.VisitAll(func(f *pflag.Flag) {
 		// print all flags for debugging
 		klog.Infof("Flag: %s, Value: %s", f.Name, f.Value.String())
@@ -60,5 +63,5 @@ func main() {
 		klog.Info("Received termination, signaling shutdown")
 		cancel()
 	}()
-	app.NewServer(port, enableTLS, tlsCert, tlsKey).Run(ctx)
+	app.NewServer(port, tlsCert != "" && tlsKey != "", tlsCert, tlsKey).Run(ctx)
 }
