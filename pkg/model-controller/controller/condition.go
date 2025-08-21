79dc263d75357a0744f260584fb2802edd2cd1a5
HunterChen <chenyuting38@h-partners.com>
2025-08-21 16:12:07 +0800

    Fix model condition handlers to retrieve model before updating status

--- pkg/model-controller/controller/condition.go
+++ pkg/model-controller/controller/condition.go
@@ -34,6 +34,10 @@ const (
 
 // setModelInitCondition sets model condition to initialized
 func (mc *ModelController) setModelInitCondition(ctx context.Context, model *registryv1alpha1.Model) error {
+	model, getError := mc.client.RegistryV1alpha1().Models(model.Namespace).Get(ctx, model.Name, metav1.GetOptions{})
+	if getError != nil {
+		return getError
+	}
 	meta.SetStatusCondition(&model.Status.Conditions, newCondition(string(registryv1alpha1.ModelStatusConditionTypeInitialized),
 		metav1.ConditionTrue, ModelInitsReason, "Model initialized"))
 	if err := mc.updateModelStatus(ctx, model); err != nil {
@@ -45,6 +49,10 @@ func (mc *ModelController) setModelInitCondition(ctx context.Context, model *reg
 
 // setModelProcessingCondition sets model condition to processing
 func (mc *ModelController) setModelProcessingCondition(ctx context.Context, model *registryv1alpha1.Model) error {
+	model, getError := mc.client.RegistryV1alpha1().Models(model.Namespace).Get(ctx, model.Name, metav1.GetOptions{})
+	if getError != nil {
+		return getError
+	}
 	meta.SetStatusCondition(&model.Status.Conditions, newCondition(string(registryv1alpha1.ModelStatusConditionTypeActive),
 		metav1.ConditionFalse, ModelProcessingReason, "Model not ready yet"))
 	if err := mc.updateModelStatus(ctx, model); err != nil {
@@ -56,6 +64,10 @@ func (mc *ModelController) setModelProcessingCondition(ctx context.Context, mode
 
 // setModelFailedCondition sets model condition to failed
 func (mc *ModelController) setModelFailedCondition(ctx context.Context, model *registryv1alpha1.Model, err error) {
+	model, getError := mc.client.RegistryV1alpha1().Models(model.Namespace).Get(ctx, model.Name, metav1.GetOptions{})
+	if getError != nil {
+		return
+	}
 	meta.SetStatusCondition(&model.Status.Conditions, newCondition(string(registryv1alpha1.ModelStatusConditionTypeFailed),
 		metav1.ConditionTrue, ModelFailedReason, err.Error()))
 	if err := mc.updateModelStatus(ctx, model); err != nil {
@@ -65,6 +77,10 @@ func (mc *ModelController) setModelFailedCondition(ctx context.Context, model *r
 
 // setModelActiveCondition sets model conditions when all Model Infers are active.
 func (mc *ModelController) setModelActiveCondition(ctx context.Context, model *registryv1alpha1.Model) error {
+	model, getError := mc.client.RegistryV1alpha1().Models(model.Namespace).Get(ctx, model.Name, metav1.GetOptions{})
+	if getError != nil {
+		return getError
+	}
 	meta.SetStatusCondition(&model.Status.Conditions, newCondition(string(registryv1alpha1.ModelStatusConditionTypeActive),
 		metav1.ConditionTrue, ModelActiveReason, "Model is ready"))
 	if err := mc.updateModelStatus(ctx, model); err != nil {
