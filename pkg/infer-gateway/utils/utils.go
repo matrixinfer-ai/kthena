7685b584911edf81c3d2862cc6761db566145807
Kuba <herojoe321@outlook.com>
2025-08-21 23:00:30 +0800

    Resolve code conflicts

--- pkg/infer-gateway/utils/utils.go
+++ pkg/infer-gateway/utils/utils.go
@@ -18,13 +18,12 @@ package utils
 
 import (
 	"fmt"
+	"os"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/klog/v2"
 	"matrixinfer.ai/matrixinfer/pkg/infer-gateway/common"
-	"matrixinfer.ai/matrixinfer/pkg/infer-gateway/scheduler/plugins/conf"
-	"sigs.k8s.io/yaml"
 )
 
 var (
@@ -102,85 +101,6 @@ func GetPromptString(chatMessage common.ChatMessage) string {
 		result += fmt.Sprintf("<|im_start|>%s\n%s<|im_end|>\n", msg.Role, msg.Content)
 	}
 	return result
-}
-
-func LoadSchedulerConfig(configMapPath string) (map[string]int, []string, map[string]runtime.RawExtension, error) {
-	data, err := os.ReadFile(configMapPath)
-	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to read config file %s: %w", configMapPath, err)
-	}
-	var kubeSchedulerConfiguration conf.SchedulerConfiguration
-	if err := yaml.Unmarshal(data, &kubeSchedulerConfiguration); err != nil {
-		klog.Errorf("failed to Unmarshal schedulerConfiguration: %v", err)
-		return nil, nil, nil, fmt.Errorf("failed to Unmarshal schedulerConfiguration: %v", err)
-	}
-
-	scorePluginMap, filterPlugins, err := unmarshalPlugins(&kubeSchedulerConfiguration)
-	if err != nil {
-		klog.Errorf("failed to Unmarshal Plugins: %v", err)
-		return nil, nil, nil, fmt.Errorf("failed to Unmarshal Plugins: %v", err)
-	}
-
-	// Check for random plugin conflicts and remove random plugin if needed
-	scorePluginMap = handleRandomPluginConflicts(scorePluginMap)
-
-	pluginsArgMap, err := unmarshalPluginsConfig(&kubeSchedulerConfiguration)
-	if err != nil {
-		klog.Errorf("failed to Unmarshal PluginsConfig: %v", err)
-		return nil, nil, nil, fmt.Errorf("failed to Unmarshal PluginsConfig: %v", err)
-	}
-
-	return scorePluginMap, filterPlugins, pluginsArgMap, nil
-}
-
-// handleRandomPluginConflicts checks if random plugin is configured with other score plugins
-// and removes the random plugin while logging a warning if conflicts are detected
-func handleRandomPluginConflicts(scorePluginMap map[string]int) map[string]int {
-	const randomPluginName = "random"
-
-	// Check if random plugin exists using direct map lookup
-	_, hasRandomPlugin := scorePluginMap[randomPluginName]
-
-	// Check if there are other plugins besides random
-	hasOtherScorePlugins := len(scorePluginMap) > 1 && hasRandomPlugin
-
-	// If both random and other score plugins are configured, remove random plugin and warn
-	if hasRandomPlugin && hasOtherScorePlugins {
-		klog.Warningf("Random plugin is configured along with other score plugins. Random plugin will be removed as it should be used independently. " +
-			"Mixing random scores with meaningful scores defeats the purpose of intelligent scheduling.")
-
-		delete(scorePluginMap, randomPluginName)
-	}
-
-	return scorePluginMap
-}
-
-func unmarshalPlugins(schedulerConfig *conf.SchedulerConfiguration) (map[string]int, []string, error) {
-	var filterPlugins []string
-	scorePluginMap := make(map[string]int)
-
-	if len(schedulerConfig.Plugins.Score.Enabled) > 0 {
-		for _, plugin := range schedulerConfig.Plugins.Score.Enabled {
-			scorePluginMap[plugin.Name] = plugin.Weight
-		}
-	}
-
-	if len(schedulerConfig.Plugins.Filter.Enabled) > 0 {
-		filterPlugins = schedulerConfig.Plugins.Filter.Enabled
-	}
-	return scorePluginMap, filterPlugins, nil
-}
-
-func unmarshalPluginsConfig(schedulerConfig *conf.SchedulerConfiguration) (map[string]runtime.RawExtension, error) {
-	pluginsArgMap := make(map[string]runtime.RawExtension)
-
-	if len(schedulerConfig.PluginConfig) > 0 {
-		for _, pluginArg := range schedulerConfig.PluginConfig {
-			pluginsArgMap[pluginArg.Name] = pluginArg.Args
-		}
-	}
-
-	return pluginsArgMap, nil
 }
 
 func LoadEnv(key, defaultValue string) string {
