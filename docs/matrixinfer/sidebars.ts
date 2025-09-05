10c719c3fca5afa67fcbceb51272e5df9dd79ffb
lizhencheng <lizhencheng6@huawei.com>
2025-09-05 10:51:03 +0800

    [Doc] add modelinfer scaling doc (#307)
    * add modelinfer scaling doc
    
    Signed-off-by: LiZhenCheng9527 <lizhencheng6@huawei.com>
    
    * fix build error
    
    Signed-off-by: LiZhenCheng9527 <lizhencheng6@huawei.com>
    
    ---------
    
    Signed-off-by: LiZhenCheng9527 <lizhencheng6@huawei.com>

--- docs/matrixinfer/sidebars.ts
+++ docs/matrixinfer/sidebars.ts
@@ -41,7 +41,6 @@ const sidebars: SidebarsConfig = {
       items: [
         'general/prometheus',
         'general/certmanager',
-        'general/ratelimit',
         'general/faq',
       ],
     },
@@ -63,6 +62,7 @@ const sidebars: SidebarsConfig = {
         'developer-guide/release',
         'developer-guide/modelinfer-rolling-update',
         'developer-guide/ci',
+        'developer-guide/modelinfer-scaling'
       ],
     },
     {
