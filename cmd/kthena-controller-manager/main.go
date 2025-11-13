453b6c8944d0f6f598079e18952a36673c8f09d2
Lu Ma <malu17@huawei.com>
2025-11-13 09:23:07 +0800

    cert bug fix
    Signed-off-by: Lu Ma <malu17@huawei.com>

--- cmd/kthena-controller-manager/main.go
+++ cmd/kthena-controller-manager/main.go
@@ -66,7 +66,7 @@ func main() {
 	pflag.IntVar(&wc.webhookTimeout, "webhook-timeout", 30, "Timeout for webhook operations in seconds")
 	pflag.BoolVar(&wc.autoGenerateCert, "auto-generate-cert", true, "If true, automatically generate self-signed certificate if not exists")
 	pflag.StringVar(&wc.certSecretName, "cert-secret-name", "kthena-webhook-certs", "Name of the secret to store auto-generated certificates")
-	pflag.StringVar(&wc.serviceName, "service-name", "kthena-webhook", "Service name for the webhook server")
+	pflag.StringVar(&wc.serviceName, "service-name", "kthena-controller-manager-webhook", "Service name for the webhook server")
 	pflag.BoolVar(&cc.EnableLeaderElection, "leader-elect", false, "Enable leader election for controller. "+
 		"Enabling this will ensure there is only one active controller. Default is false.")
 	pflag.IntVar(&cc.Workers, "workers", 5, "number of workers to run. Default is 5")
